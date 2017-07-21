9e118bd6bbc538aad7fb402ef3b7ef66da7ad7c9
Nate Chastain <chastain616@gmail.com>
2017-07-21 01:31:27 -0400

    dressing up today view

--- src/components/Day.js
+++ src/components/Day.js
@@ -50,22 +50,25 @@ class Day extends React.Component {
     return dayNotes.map(function (note, idx) {
       return (
         <View style={styles.dayEntry} key={idx} >
-          <View style={{ flex: 1 }}>
+          <View style={{ flex: 1, alignSelf: 'stretch' }}>
             <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center' }}>
-              {note.tagIDs && note.tagIDs.length > 0 && <View style={{ width: 100, flexDirection: 'column', justifyContent: 'center' }}>
-                {note.tagIDs.map((tagID, idx) => (
-                  <TouchableOpacity key={idx} style={{marginBottom: 10, marginTop: 10}} onPress={() => that.handleHashtagLookup(that.findTagByID(tagID))}>
-                    <Text style={{ color: '#a083c4', fontWeight: 'bold', fontSize: 16 }}>#{that.findTagByID(tagID).text}</Text>
-                  </TouchableOpacity>
-                ))}
-              </View>}
-              <Text style={{ flex: 1 }}>{note.text.replace(/(\B#\w\w+\w+)/g, '')}</Text>
+              <View style={{ marginRight: 5, alignItems: 'flex-start', flex: 1}}><TouchableOpacity style={{justifyContent: 'center', padding: 12, borderRadius: 5, backgroundColor: '#a083c4'}}><Text style={{color: 'white', fontWeight: 'bold'}}>Hi</Text></TouchableOpacity></View>
+              <Text style={{ flex: 4 }}>{note.text.replace(/(\B#\w\w+\w+)/g, '')}</Text>
             </View>
           </View>
         </View>
       )
     })
   }
+
+
+  // {note.tagIDs && note.tagIDs.length > 0 && <View style={{ width: 100 }}>
+  //               {note.tagIDs.map((tagID, idx) => (
+  //                 <TouchableOpacity key={idx} onPress={() => that.handleHashtagLookup(that.findTagByID(tagID))}>
+  //                   <Text style={{ color: '#a083c4', fontWeight: 'bold', fontSize: 16 }}>#{that.findTagByID(tagID).text}</Text>
+  //                 </TouchableOpacity>
+  //               ))}
+  //             </View>}
 
   addProgress () {
     Actions.EntryAdd()
@@ -82,7 +85,7 @@ class Day extends React.Component {
     const dayEntries = entriesArr.filter(entry => isFromToday(entry.date))
     const dayNotes = notesArr.filter(note => isFromToday(note.date))
     return (
-      <View style={{marginTop: 62, marginBottom: 50}}>
+      <View style={{flex: 1, backgroundColor: '#a083c4'}}>
         <View style={styles.topBar}>
           <TouchableOpacity style={styles.topBarButton} onPress={this.addNote.bind(this)}>
             <Text style={{fontSize: 14, fontWeight: 'bold', color: 'white'}}>+ Add note</Text>
@@ -92,7 +95,7 @@ class Day extends React.Component {
           </TouchableOpacity>
         </View>
         <ScrollView contentContainerStyle={styles.container}>
-          <View style={{alignSelf: 'stretch'}}>
+          <View style={{flex: 1}}>
             {this.buildDayEntries(dayEntries)}
             {this.buildDayNotes(dayNotes)}
           </View>
@@ -104,15 +107,18 @@ class Day extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    justifyContent: 'center',
-    padding: 0,
+    justifyContent: 'flex-start',
+    padding: 10,
     alignSelf: 'stretch',
-    marginBottom: 65,
-    marginTop: 10,
-    backgroundColor: '#eee',
+    flex: 1,
+    backgroundColor: '#a083c4',
+    marginBottom: 50,
   },
   topBar: {
-    padding: 10,
+    paddingBottom: 5,
+    paddingTop: 70,
+    paddingLeft: 5,
+    paddingRight: 5,
     alignItems: 'center',
     backgroundColor: '#eee',
     flexDirection: 'row',
@@ -130,7 +136,7 @@ const styles = StyleSheet.create({
     width: 100,
     alignItems: 'flex-start',
     justifyContent: 'center',
-    alignSelf: 'stretch',
+    alignSelf: 'center',
   },
   entryDuration: {
     color: '#a083c4',
@@ -138,13 +144,23 @@ const styles = StyleSheet.create({
   },
   dayEntry: {
     backgroundColor: 'white',
-    alignSelf: 'stretch',
-    height: 80,
+    marginTop: 5,
+    marginBottom: 5,
+    marginLeft: 0,
+    marginRight: 0,
+    borderRadius: 5,
     alignItems: 'center',
+    alignSelf: 'stretch',
+    justifyContent: 'center',
     borderBottomWidth: 1,
     borderColor: 'lightgray',
     padding: 20,
-    flexDirection: 'row'
+    paddingTop: 40,
+    paddingBottom: 40,
+    flexDirection: 'column',
+    shadowOffset: { width: 4,  height: 4,  },
+    shadowColor: '#555',
+    shadowOpacity: 0.3,
   },
   welcome: {
     fontSize: 20,
